4f8735177461d7dc2bca428d3dfd5196d47633ca
yufeiminds <yufeiminds@gmail.com>
2018-10-18 21:05:13 +0800

    fix uhost & vip attrtibutes

--- ucloud/version/version.go
+++ ucloud/version/version.go
@@ -4,4 +4,8 @@ Package version is the version of sdk
 package version
 
 // Version see also semantic version: https://semver.org/
+<<<<<<< HEAD
 const Version = "0.5.5"
+=======
+const Version = "0.5.4"
+>>>>>>> upstream/master
