763e0dbb2fc6227fc47e15e50db90518b629fb0f
cloudSjhan <13210081836@163.com>
2018-11-14 11:33:43 +0800

    Add DescribeOrderDetailInfo to SDK

---
+++ services/ubill/client.go
@@ -0,0 +1,19 @@
+package ubill
+
+import (
+	"github.com/ucloud/ucloud-sdk-go/ucloud"
+	"github.com/ucloud/ucloud-sdk-go/ucloud/auth"
+)
+
+// UBillClient is the client of UBill
+type UBillClient struct {
+	client *ucloud.Client
+}
+
+// NewClient will return a instance of UBillClient
+func NewClient(config *ucloud.Config, credential *auth.Credential) *UBillClient {
+	client := ucloud.NewClient(config, credential)
+	return &UBillClient{
+		client: client,
+	}
+}
