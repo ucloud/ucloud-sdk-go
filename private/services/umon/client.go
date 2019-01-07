e978403e73e2378fa9b67501ffa773900ee5d97a
yufeiminds <yufeiminds@gmail.com>
2019-01-07 12:06:50 +0800

    add allowance and vpc action

---
+++ private/services/umon/client.go
@@ -0,0 +1,19 @@
+package umon
+
+import (
+	"github.com/ucloud/ucloud-sdk-go/ucloud"
+	"github.com/ucloud/ucloud-sdk-go/ucloud/auth"
+)
+
+// UMonClient is the client of UMon
+type UMonClient struct {
+	client *ucloud.Client
+}
+
+// NewClient will return a instance of UMonClient
+func NewClient(config *ucloud.Config, credential *auth.Credential) *UMonClient {
+	client := ucloud.NewClient(config, credential)
+	return &UMonClient{
+		client: client,
+	}
+}
