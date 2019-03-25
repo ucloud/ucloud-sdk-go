6fccc4782f5462f3235876e556aed49f128743a4
mingsheng.su <minghsheng.su@ucloud.cn>
2019-03-25 14:15:16 +0800

    add three api to private services

---
+++ private/services/vpc/types_ip_info.go
@@ -0,0 +1,28 @@
+package vpc
+
+/*
+IpInfo -
+
+this model is auto created by ucloud code generater for open api,
+you can also see https://docs.ucloud.cn for detail.
+*/
+type IpInfo struct {
+
+	//
+	Ip string
+
+	//
+	Mask string
+
+	//
+	Gateway string
+
+	//
+	Mac string
+
+	//
+	SubnetId string
+
+	//
+	VPCId string
+}
