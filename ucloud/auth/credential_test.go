ac3398e1e721e0354f43712f098d689f0072035e
yufei.li <yufeiminds@gmail.com>
2019-06-26 21:17:16 +0800

    fix credential for params with long array

--- ucloud/auth/credential_test.go
+++ ucloud/auth/credential_test.go
@@ -25,6 +25,18 @@ func TestCredential_CreateSign(t *testing.T) {
 			args{testCredentialCreateSignQuery00},
 			"4f9ef5df2abab2c6fccd1e9515cb7e2df8c6bb65",
 		},
+		{
+			"unorder",
+			fields{"ucloudsomeone@example.com1296235120854146120", "46f09bb9fab4f12dfc160dae12273d5332b5debe"},
+			args{testCredentialCreateSignQuery01},
+			"4f9ef5df2abab2c6fccd1e9515cb7e2df8c6bb65",
+		},
+		{
+			"noPublicKey",
+			fields{"ucloudsomeone@example.com1296235120854146120", "46f09bb9fab4f12dfc160dae12273d5332b5debe"},
+			args{testCredentialCreateSignQuery02},
+			"4f9ef5df2abab2c6fccd1e9515cb7e2df8c6bb65",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -34,6 +46,46 @@ func TestCredential_CreateSign(t *testing.T) {
 			}
 			if got := c.CreateSign(tt.args.query); got != tt.want {
 				t.Errorf("Credential.CreateSign() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCredential_BuildCredentialedQuery(t *testing.T) {
+	type fields struct {
+		PublicKey  string
+		PrivateKey string
+	}
+	type args struct {
+		params map[string]string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{
+			"standard",
+			fields{"ucloudsomeone@example.com1296235120854146120", "46f09bb9fab4f12dfc160dae12273d5332b5debe"},
+			args{testCredentialBuildCredentialedQuery01},
+			"4f9ef5df2abab2c6fccd1e9515cb7e2df8c6bb65",
+		},
+		{
+			"longArray",
+			fields{"ucloudsomeone@example.com1296235120854146120", "46f09bb9fab4f12dfc160dae12273d5332b5debe"},
+			args{testCredentialBuildCredentialedQuery02},
+			"6bc32642779f8cb0bb3d3f128b1c0688abf9a1a3",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Credential{
+				PublicKey:  tt.fields.PublicKey,
+				PrivateKey: tt.fields.PrivateKey,
+			}
+			if got := c.BuildCredentialedQuery(tt.args.params); !strings.Contains(got, tt.want) {
+				t.Errorf("Credential.BuildCredentialedQuery() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -52,3 +104,61 @@ var testCredentialCreateSignQuery00 = strings.Join(strings.Split(`Action=CreateU
 &Quantity=1
 &Region=cn-bj2
 &Zone=cn-bj2-04`, "\n"), "")
+
+var testCredentialCreateSignQuery01 = strings.Join(strings.Split(`Action=CreateUHostInstance
+&CPU=2
+&Region=cn-bj2
+&DiskSpace=10
+&LoginMode=Password
+&ChargeType=Month
+&ImageId=f43736e1-65a5-4bea-ad2e-8a46e18883c2
+&PublicKey=ucloudsomeone%40example.com1296235120854146120
+&Memory=2048
+&Password=VUNsb3VkLmNu
+&Name=Host01
+&Quantity=1
+&Zone=cn-bj2-04`, "\n"), "")
+
+var testCredentialCreateSignQuery02 = strings.Join(strings.Split(`Action=CreateUHostInstance
+&CPU=2
+&ChargeType=Month
+&DiskSpace=10
+&ImageId=f43736e1-65a5-4bea-ad2e-8a46e18883c2
+&LoginMode=Password
+&Memory=2048
+&Name=Host01
+&Password=VUNsb3VkLmNu
+&Quantity=1
+&Region=cn-bj2
+&Zone=cn-bj2-04`, "\n"), "")
+
+var testCredentialBuildCredentialedQuery01 = map[string]string{
+	"Action":     "CreateUHostInstance",
+	"CPU":        "2",
+	"ChargeType": "Month",
+	"DiskSpace":  "10",
+	"ImageId":    "f43736e1-65a5-4bea-ad2e-8a46e18883c2",
+	"LoginMode":  "Password",
+	"Memory":     "2048",
+	"Name":       "Host01",
+	"Password":   "VUNsb3VkLmNu",
+	"Quantity":   "1",
+	"Region":     "cn-bj2",
+	"Zone":       "cn-bj2-04",
+}
+
+var testCredentialBuildCredentialedQuery02 = map[string]string{
+	"Action":      "AllocateBackendBatch",
+	"Backends.0":  "foo",
+	"Backends.1":  "bar",
+	"Backends.2":  "42",
+	"Backends.3":  "foo",
+	"Backends.4":  "bar",
+	"Backends.5":  "42",
+	"Backends.6":  "foo",
+	"Backends.7":  "bar",
+	"Backends.8":  "42",
+	"Backends.9":  "foo",
+	"Backends.10": "bar",
+	"Backends.11": "42",
+}
