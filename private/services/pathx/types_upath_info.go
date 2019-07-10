da733d78b6dec0c421b4e1e7bcfd1e6e1643b0bb
mingsheng.su <shawn.mssu@gmail.com>
2019-07-10 20:12:54 +0800

    add private api about pathx

---
+++ private/services/pathx/types_upath_info.go
@@ -0,0 +1,34 @@
+package pathx
+
+/*
+UPathInfo - 加速线路信息
+
+this model is auto created by ucloud code generater for open api,
+you can also see https://docs.ucloud.cn for detail.
+*/
+type UPathInfo struct {
+
+	// UPath名字
+	Name string
+
+	// UPath ID 号
+	UPathId string
+
+	// 带宽
+	Bandwidth string
+
+	// 线路ID
+	LineId string
+
+	// 与该UPath绑定的UGA列表
+	UGAList []UGAAInfo
+
+	// UPath创建的时间
+	CreateTime int
+
+	// UPath的过期时间
+	ExpireTime int
+
+	// 支付方式
+	ChargeType string
+}
