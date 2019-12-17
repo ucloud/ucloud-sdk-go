f89b318137158ac43a26b771a6737341200441e3
mingsheng.su <shawn.mssu@gmail.com>
2019-12-17 13:53:12 +0800

    update generic request

--- ucloud/request/generic.go
+++ ucloud/request/generic.go
@@ -19,20 +19,20 @@ func (r *BaseGenericRequest) SetPayload(m map[string]interface{}) {
 
 func (r BaseGenericRequest) Payload() map[string]interface{} {
 	m := make(map[string]interface{})
-	if len(r.GetRegion()) != 0 {
-		m["Region"] = r.GetRegion()
+	if len(r.CommonBase.GetRegion()) != 0 {
+		m["Region"] = r.CommonBase.GetRegion()
 	}
 
-	if len(r.GetZone()) != 0 {
-		m["Zone"] = r.GetZone()
+	if len(r.CommonBase.GetZone()) != 0 {
+		m["Zone"] = r.CommonBase.GetZone()
 	}
 
-	if len(r.GetAction()) != 0 {
-		m["Action"] = r.GetAction()
+	if len(r.CommonBase.GetAction()) != 0 {
+		m["Action"] = r.CommonBase.GetAction()
 	}
 
-	if len(r.GetProjectId()) != 0 {
-		m["ProjectId"] = r.GetProjectId()
+	if len(r.CommonBase.GetProjectId()) != 0 {
+		m["ProjectId"] = r.CommonBase.GetProjectId()
 	}
 
 	for k, v := range r.payload {
@@ -40,4 +40,36 @@ func (r BaseGenericRequest) Payload() map[string]interface{} {
 	}
 
 	return m
+}
+
+func (r *BaseGenericRequest) GetAction() string {
+	if r.payload["Action"] != nil {
+		return r.payload["Action"].(string)
+	}
+
+	return r.CommonBase.GetAction()
+}
+
+func (r *BaseGenericRequest) GetRegion() string {
+	if r.payload["Region"] != nil {
+		return r.payload["Region"].(string)
+	}
+
+	return r.CommonBase.GetRegion()
+}
+
+func (r *BaseGenericRequest) GetZone() string {
+	if r.payload["Zone"] != nil {
+		return r.payload["Zone"].(string)
+	}
+
+	return r.CommonBase.GetZone()
+}
+
+func (r *BaseGenericRequest) GetProjectId() string {
+	if r.payload["ProjectId"] != nil {
+		return r.payload["ProjectId"].(string)
+	}
+
+	return r.CommonBase.GetProjectId()
 }
