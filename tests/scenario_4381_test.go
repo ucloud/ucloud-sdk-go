b816b71a84089178a471ee160575c54e91bc8af6
UCloud <uweb-bot@ucloud.cn>
2020-08-12 11:02:03 +0800

    auto codegen for ULB

--- tests/scenario_4381_test.go
+++ tests/scenario_4381_test.go
@@ -24,7 +24,7 @@ func TestScenario4381(t *testing.T) {
 				"Zone":   "cn-bj2-02",
 			}
 		},
-		Owners: []string{"li.wei@ucloud.cn"},
+		Owners: []string{"arno.gao@ucloud.cn"},
 		Title:  "外网-ulb4自动化回归-枚举参数验证-05",
 		Steps: []*driver.Step{
 			testStep4381CreateULB01,
