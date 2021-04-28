492648b14a52f2353d92708914b11e843049e919
UCloud <uweb-bot@ucloud.cn>
2021-04-28 19:03:53 +0800

    auto codegen for UDDB

--- tests/setup_test.go
+++ tests/setup_test.go
@@ -57,6 +57,7 @@ import (
 	"github.com/ucloud/ucloud-sdk-go/services/cube"
 	"github.com/ucloud/ucloud-sdk-go/services/stepflow"
 	"github.com/ucloud/ucloud-sdk-go/services/ucdn"
+	"github.com/ucloud/ucloud-sdk-go/services/uddb"
 	"github.com/ucloud/ucloud-sdk-go/services/udts"
 	"github.com/ucloud/ucloud-sdk-go/services/uec"
 	"github.com/ucloud/ucloud-sdk-go/services/uk8s"
@@ -162,6 +163,9 @@ func testSetup() {
 	}))
 	spec.AddFixture("UDB", driver.SetupClientFixture(func() (ucloud.ServiceClient, error) {
 		return udb.NewClient(&cfg, &credential), nil
+	}))
+	spec.AddFixture("UDDB", driver.SetupClientFixture(func() (ucloud.ServiceClient, error) {
+		return uddb.NewClient(&cfg, &credential), nil
 	}))
 	spec.AddFixture("UDPN", driver.SetupClientFixture(func() (ucloud.ServiceClient, error) {
 		return udpn.NewClient(&cfg, &credential), nil
