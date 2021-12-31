1c9789a615640a225d6d58b4f881d8f7e40c2847
UCloud <uweb-bot@ucloud.cn>
2021-12-31 18:53:20 +0800

    auto codegen for UDB

--- services/udb/models.go
+++ services/udb/models.go
@@ -451,3 +451,15 @@ type UDBTypeSet struct {
 	// DB类型id，mysql/mongodb按版本细分各有一个id, 目前id的取值范围为[1,7],数值对应的版本如下： 1：mysql-5.5，2：mysql-5.1，3：percona-5.5 4：mongodb-2.4，5：mongodb-2.6，6：mysql-5.6， 7：percona-5.6
 	DBTypeId string
 }
+
+/*
+ConnNumMap - db实例ip和连接数信息
+*/
+type ConnNumMap struct {
+
+	// 客户端IP
+	Ip string
+
+	// 该Ip连接数
+	Num int
+}
