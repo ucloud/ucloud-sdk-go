c5a4a727d22badb103b2d0427a87a015d8519141
UCloud <uweb-bot@ucloud.cn>
2022-01-12 16:05:05 +0800

    auto codegen for UDTS

--- services/udts/models.go
+++ services/udts/models.go
@@ -42,21 +42,6 @@ type CheckUDTSTaskResult struct {
 }
 
 /*
-PolicyData - Policy 数据结构
-*/
-type PolicyData struct {
-
-	// 列名
-	Column string
-
-	// 与上面类型对应的值， 比如“1”， “now()”, "Nash".
-	Data string
-
-	// 可选值为 CSVData, Function, Fixed
-	Type string
-}
-
-/*
 SyncData - 增量同步数据
 */
 type SyncData struct {
@@ -71,91 +56,22 @@ type SyncData struct {
 	BinlogPos int
 
 	// 分配给UDTS task的server ID, 必须在MySQL集群中唯一
-	ServerId int
+	ServerId int `deprecated:"true"`
 }
 
 /*
-TiDBNode - TiDB 结点
+PolicyData - Policy 数据结构
 */
-type TiDBNode struct {
+type PolicyData struct {
 
-	// DB 名字， 长度不超过63个字符
-	DataBase string
+	// 列名
+	Column string
 
-	// 地域，只有当 Host 为 UCloud 用户内网地址的时候需要提供
-	DataRegion string
+	// 与上面类型对应的值， 比如“1”， “now()”, "Nash".
+	Data string
 
-	// 数据库地址，长度不能超过 60个字符
-	Host string
-
-	// 数据库密码，长度不起来32个字符
-	Password string
-
-	// 数据库端口，端口范围 1-65535
-	Port int
-
-	// 子网 ID, 只有当 Host 为 UCloud 用户内网地址并且源目属于不同的地域的时候需要提供。
-	SubnetId string
-
-	// 表名， 长度不超过64个字符
-	Table string
-
-	// 数据库用户名，长度不能超过 32个字符
-	User string
-
-	// VPC 资源ID, 只有当 Host 为 UCloud 用户内网地址的时候需要提供。
-	VPCId string
-}
-
-/*
-RedisNode - Redis节点
-*/
-type RedisNode struct {
-
-	// 数据库地址，只填写主(master)地址，集群模式下，多个地址用 ; 相连
-	Address string
-
-	// 数据库所在的地域。 只有当 Host 为 UCloud 用户内网地址的时候需要提供
-	DataRegion string
-
-	// Redis2Redis全量迁移是否使用rump，默认是dump-restore
-	IsRump string
-
-	// redis密码
-	Password string
-
-	// 子网 ID, 只有当 Host 为 UCloud 用户内网地址并且源目属于不同的地域的时候需要提供
-	SubnetId string
-
-	// redis模式
+	// 可选值为 CSVData, Function, Fixed
 	Type string
-
-	// 数据库所在机器的 VPCId, 只有内网跨域迁移的时候需要提供
-	VPCId string
-}
-
-/*
-CSVNode - CSV 结点
-*/
-type CSVNode struct {
-
-	// 如果 DupAction 为 ignore或者replace,  并且需要调整列的顺序的时候使用。 以逗号分割的列名字符串。
-	Columns string
-
-	// 当加载重复数据的时候所采取的行为，有效值有 ignore - 忽略， replace - 替换， update - 更新。 默认为replace
-	DupAction string
-
-	// 数据迁移的时候是否保留原有数据， 默认为 false 不保留
-	KeepExistData bool
-
-	// 如果 DupAction 为 update, 并且在插入数据的同时想给一些列赋予特定的值的时候使用。
-	SetPolicy []PolicyData
-
-	// 数据路径
-	URL string
-
-	// 如果 DupAction 为 update, 并且不想用CSV数据完整替换原有数据的时候使用。
-	UpdatePolicy []PolicyData
 }
 
 /*
@@ -246,6 +162,90 @@ type UFileNode struct {
 
 	// 公钥
 	PublicKey string
+}
+
+/*
+TiDBNode - TiDB 结点
+*/
+type TiDBNode struct {
+
+	// DB 名字， 长度不超过63个字符
+	DataBase string
+
+	// 地域，只有当 Host 为 UCloud 用户内网地址的时候需要提供
+	DataRegion string
+
+	// 数据库地址，长度不能超过 60个字符
+	Host string
+
+	// 数据库密码，长度不起来32个字符
+	Password string
+
+	// 数据库端口，端口范围 1-65535
+	Port int
+
+	// 子网 ID, 只有当 Host 为 UCloud 用户内网地址并且源目属于不同的地域的时候需要提供。
+	SubnetId string
+
+	// 表名， 长度不超过64个字符
+	Table string
+
+	// 数据库用户名，长度不能超过 32个字符
+	User string
+
+	// VPC 资源ID, 只有当 Host 为 UCloud 用户内网地址的时候需要提供。
+	VPCId string
+}
+
+/*
+RedisNode - Redis节点
+*/
+type RedisNode struct {
+
+	// 数据库地址，只填写主(master)地址，集群模式下，多个地址用 ; 相连
+	Address string
+
+	// 数据库所在的地域。 只有当 Host 为 UCloud 用户内网地址的时候需要提供
+	DataRegion string
+
+	// Redis2Redis全量迁移是否使用rump，默认是dump-restore
+	IsRump string
+
+	// redis密码
+	Password string
+
+	// 子网 ID, 只有当 Host 为 UCloud 用户内网地址并且源目属于不同的地域的时候需要提供
+	SubnetId string
+
+	// redis模式
+	Type string
+
+	// 数据库所在机器的 VPCId, 只有内网跨域迁移的时候需要提供
+	VPCId string
+}
+
+/*
+CSVNode - CSV 结点
+*/
+type CSVNode struct {
+
+	// 如果 DupAction 为 ignore或者replace,  并且需要调整列的顺序的时候使用。 以逗号分割的列名字符串。
+	Columns string
+
+	// 当加载重复数据的时候所采取的行为，有效值有 ignore - 忽略， replace - 替换， update - 更新。 默认为replace
+	DupAction string
+
+	// 数据迁移的时候是否保留原有数据， 默认为 false 不保留
+	KeepExistData bool
+
+	// 如果 DupAction 为 update, 并且在插入数据的同时想给一些列赋予特定的值的时候使用。
+	SetPolicy []PolicyData
+
+	// 数据路径
+	URL string
+
+	// 如果 DupAction 为 update, 并且不想用CSV数据完整替换原有数据的时候使用。
+	UpdatePolicy []PolicyData
 }
 
 /*
@@ -376,11 +376,23 @@ ListDataItem - 返回列表的一个 Task 的信息
 */
 type ListDataItem struct {
 
+	// 是否自动续费，枚举：Yes/ No
+	AutoRenew string
+
+	// 资源付费类型，枚举：Dynamic/ Month/ Year/ Trial/ Day
+	ChargeType string
+
 	// 创建时间
 	CreateTime int
 
 	// 当前失败重试次数
 	CurRetryCount int
+
+	// 资源有效期时间戳
+	ExpireTime int
+
+	// 资源是否过期，枚举：Yes/ No
+	IsExpire string
 
 	// 最大失败重试次数
 	MaxRetryCount int
@@ -390,6 +402,9 @@ type ListDataItem struct {
 
 	// 全量迁移进度信息，增量迁移时为空
 	Progress Progress
+
+	// 服务类型， small, medium, large
+	ServiceType string
 
 	// 任务状态
 	Status string
