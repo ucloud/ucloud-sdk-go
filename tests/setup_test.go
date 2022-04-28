93c386d8dcb92a1bc301b3bb3255d7754e66b3d5
UCloud <uweb-bot@ucloud.cn>
2022-04-28 17:54:16 +0800

    auto codegen for UPhone (#349)

--- tests/setup_test.go
+++ tests/setup_test.go
@@ -66,6 +66,7 @@ import (
 	"github.com/ucloud/ucloud-sdk-go/services/uk8s"
 	"github.com/ucloud/ucloud-sdk-go/services/umedia"
 	"github.com/ucloud/ucloud-sdk-go/services/unvs"
+	"github.com/ucloud/ucloud-sdk-go/services/uphone"
 )
 
 func TestMain(m *testing.M) {
@@ -218,6 +219,9 @@ func testSetup() {
 	}))
 	spec.AddFixture("UPHost", driver.SetupClientFixture(func() (ucloud.ServiceClient, error) {
 		return uphost.NewClient(&cfg, &credential), nil
+	}))
+	spec.AddFixture("UPhone", driver.SetupClientFixture(func() (ucloud.ServiceClient, error) {
+		return uphone.NewClient(&cfg, &credential), nil
 	}))
 	spec.AddFixture("VPC", driver.SetupClientFixture(func() (ucloud.ServiceClient, error) {
 		return vpc.NewClient(&cfg, &credential), nil
