3a9d0be59e37e1f4611664a1f387a969d75eb209
UCloud <uweb-bot@ucloud.cn>
2023-03-17 22:26:51 +0800

    sdk: rolling update for 0.21.52 (#377)

--- services/ucdn/models.go
+++ services/ucdn/models.go
@@ -3,6 +3,15 @@
 package ucdn
 
 /*
+AccessConf - 访问控制
+*/
+type AccessConf struct {
+
+	// 多个ip用逗号隔开
+	IpBlacklist string
+}
+
+/*
 CacheConf - 缓存配置
 */
 type CacheConf struct {
@@ -27,15 +36,6 @@ type CacheConf struct {
 
 	// 路径模式，支持正则
 	PathPattern string
-}
-
-/*
-AccessConf - 访问控制
-*/
-type AccessConf struct {
-
-	// 多个ip用逗号隔开
-	IpBlacklist string
 }
 
 /*
@@ -549,21 +549,6 @@ type BandwidthTrafficInfo struct {
 }
 
 /*
-CacheKeyInfo - 忽略参数缓存配置
-*/
-type CacheKeyInfo struct {
-
-	// 是否忽略
-	Ignore bool
-
-	// 路径模式，支持正则
-	PathPattern string
-
-	// 自定义变量,以$符号开头，多个变量用加号(+)连接，$querystring表示所有变量
-	QueryString string
-}
-
-/*
 ReferConf - refer配置
 */
 type ReferConf struct {
@@ -576,6 +561,21 @@ type ReferConf struct {
 
 	// Refer防盗链配置  0白名单，1黑名单
 	ReferType int
+}
+
+/*
+CacheKeyInfo - 忽略参数缓存配置
+*/
+type CacheKeyInfo struct {
+
+	// 是否忽略
+	Ignore bool
+
+	// 路径模式，支持正则
+	PathPattern string
+
+	// 自定义变量,以$符号开头，多个变量用加号(+)连接，$querystring表示所有变量
+	QueryString string
 }
 
 /*
@@ -636,6 +636,18 @@ type AdvancedConf struct {
 }
 
 /*
+AccessControlConf - 访问控制配置参数
+*/
+type AccessControlConf struct {
+
+	// ip黑名单，多个ip，可表示为：IpBlackList.0=1.1.1.1，IpBlackList.1=2.2.2.2
+	IpBlackList []string
+
+	// refer配置
+	ReferConf ReferConf
+}
+
+/*
 CacheAllConfig - 缓存相关的配置
 */
 type CacheAllConfig struct {
@@ -651,18 +663,6 @@ type CacheAllConfig struct {
 
 	// 状态码缓存配置列表，参见CacheConf
 	HttpCodeCacheList []CacheConf
-}
-
-/*
-AccessControlConf - 访问控制配置参数
-*/
-type AccessControlConf struct {
-
-	// ip黑名单，多个ip，可表示为：IpBlackList.0=1.1.1.1，IpBlackList.1=2.2.2.2
-	IpBlackList []string
-
-	// refer配置
-	ReferConf ReferConf
 }
 
 /*
